03a67d0bdc13f55993e28b9c51f1bdc1dab3ba62
J08nY <johny@neuromancer.sk>
2018-06-11 00:53:01 +0200

    Custom directory icons.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -26,6 +26,55 @@ function fish_prompt
       echo ""
     end
   end)(echo "$prompt" \
+  | sed "s|~/arch|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf187
+      echo -n (flash_off)
+    end)"|; s|~/aud|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf001
+      echo -n (flash_off)
+    end)"|; s|~/bin|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf120
+      echo -n (flash_off)
+    end)"|; s|~/cloud|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf0c2
+      echo -n (flash_off)
+    end)"|; s|~/dev|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf121
+      echo -n (flash_off)
+    end)"|; s|~/doc|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf15c
+      echo -n (flash_off)
+    end)"|; s|~/down|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf019
+      echo -n (flash_off)
+    end)"|; s|~/edu|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf19d
+      echo -n (flash_off)
+    end)"|; s|~/img|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf03e
+      echo -n (flash_off)
+    end)"|; s|~/proj|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf247
+      echo -n (flash_off)
+    end)"|; s|~/pub|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf15b
+      echo -n (flash_off)
+    end)"|; s|~/vid|"(begin
+      echo -n "~/"(flash_fst)
+      printf \uf03d
+      echo -n (flash_off)
+    end)"|" \
   | sed "s|~|"(begin
       test $code -eq 0; and echo (flash_fst); or echo (flash_dim)
     end)"⌁"(flash_off)"|g" \
